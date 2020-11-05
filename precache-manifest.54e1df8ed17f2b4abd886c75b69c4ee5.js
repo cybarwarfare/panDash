13566ed024a3e32eae442796334a921065f01d41
cybarwarfare <hafismuhammed984@gmail.com>
2020-11-05 14:05:47 +0530

    Updates

---
+++ precache-manifest.54e1df8ed17f2b4abd886c75b69c4ee5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f3947eeda7ee95385bac2fde33f7120",
+    "url": "/panDash/index.html"
+  },
+  {
+    "revision": "f36a38fc4af463d87df1",
+    "url": "/panDash/static/css/2.b234b7d0.chunk.css"
+  },
+  {
+    "revision": "0bca538ecc450bad01ec",
+    "url": "/panDash/static/css/main.2626af6f.chunk.css"
+  },
+  {
+    "revision": "f36a38fc4af463d87df1",
+    "url": "/panDash/static/js/2.93a1b345.chunk.js"
+  },
+  {
+    "revision": "4da68047bfb92de2093d237ae27e73a9",
+    "url": "/panDash/static/js/2.93a1b345.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bca538ecc450bad01ec",
+    "url": "/panDash/static/js/main.a6163af8.chunk.js"
+  },
+  {
+    "revision": "c9986d656268ccbe07e9",
+    "url": "/panDash/static/js/runtime-main.1f482918.js"
+  },
+  {
+    "revision": "2d18a2c866a05604dd545a8959f467c4",
+    "url": "/panDash/static/media/PANTO.2d18a2c8.png"
+  },
+  {
+    "revision": "bc841a07000852731a734fd46efbce97",
+    "url": "/panDash/static/media/bussiness-man.bc841a07.png"
+  },
+  {
+    "revision": "325c2b6e1dea7694b24ca5d94c62f40a",
+    "url": "/panDash/static/media/countdown.325c2b6e.png"
+  },
+  {
+    "revision": "1785fa79bb07f4833eaa8ba169b94b83",
+    "url": "/panDash/static/media/funnel.1785fa79.png"
+  },
+  {
+    "revision": "e75ec37b2fbfc13a070762ce3ee56552",
+    "url": "/panDash/static/media/payment-method.e75ec37b.png"
+  }
+]);
